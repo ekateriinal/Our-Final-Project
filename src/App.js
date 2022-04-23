6a4978a36287196c2256cade8c18732ab8ab560f
ekateriinal <ekateriina.loginova@outlook.com>
2022-04-23 14:44:59 +0300

    App - example styling added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import './styles/base.scss';
+import './styles/sass-base.scss';
 import styles from './App.module.scss';
 
 const App = () => {
   return (
-   <>
-   <h1 className={styles.header}>Hello!</h1>
-   </>
+   <div className={styles.wrapper}>
+   <h1 className={styles['wrapper__header']}>Hello!</h1>
+   </div>
   )
 }
 
