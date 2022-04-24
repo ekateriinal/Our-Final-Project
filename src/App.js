a9df75ac919aded66e0c930875d76a81627b340c
ekateriinal <ekateriina.loginova@outlook.com>
2022-04-24 11:30:01 +0300

    Removed wrapper from App.js fixde Header imports

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import "./styles/base.scss";
 import "./styles/sass-base.scss";
 import styles from "./App.module.scss";
-import Heading from "./Components/Heading/Heading";
-import Wrapper from "./Components/Wrapper/Wrapper";
+import Header from "./Components/Header/Header";
+// import Wrapper from "./Components/Wrapper/Wrapper";
 
 const App = () => {
   return (
     <div className={styles["wrapper"]}>
-      <Wrapper appearance="light">
-        <Heading>Hello! Iam on the light wrapper</Heading>
-      </Wrapper>
-      <Wrapper appearance="dark">
-        <Heading>I am the best. Okey it is a joke.</Heading>
-      </Wrapper>
+        <Header></Header>
     </div>
   );
 };
