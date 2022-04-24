b4eb605d5db982ccfbe418b847d0cf41dda7d9c9
ekateriinal <ekateriina.loginova@outlook.com>
2022-04-24 14:25:47 +0300

    Header - active link change

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,30 +2,56 @@ import styles from './Header.module.scss';
 import { ReactComponent as Logo } from '../../images/logo.svg';
 import { ReactComponent as Dollar } from '../../images/dollar.svg';
 import { ReactComponent as Cart } from '../../images/cart.svg';
-
+import { useState } from 'react';
 
 const Header = () => {
+  const [activeLink, setActiveLink] = useState('w');
+
   return (
     <header className={styles.header}>
       <nav className={styles['header__nav-box']}>
         <ul className={styles['header__nav-group']}>
-          <li className={`${styles['header__nav-item']}
-              ${styles['header__nav-item--active']}
+          <li className={styles['header__nav-item']}>
+            <a onClick={() => setActiveLink('w')}
+              className={styles['header__nav-link']}
+              href='#'>
+              <span className={`
+              ${styles['header__link-content']}
+              ${activeLink === 'w' ? styles['header__link-content--active'] : ''}
               `}>
-            <a className={`${styles['header__nav-link']}`} href='#'>
-              Women</a>
+                Women
+              </span>
+            </a>
           </li>
           <li className={styles['header__nav-item']}>
-            <a className={styles['header__nav-link']} href='#'>Men</a>
+            <a onClick={() => setActiveLink('m')}
+              className={styles['header__nav-link']}
+              href='#'>
+              <span className={`
+              ${styles['header__link-content']}
+              ${activeLink === 'm' ? styles['header__link-content--active'] : ''}
+              `}>
+                Men
+              </span>
+            </a>
           </li>
           <li className={styles['header__nav-item']}>
-            <a className={styles['header__nav-link']} href='#'>Kids</a>
+            <a onClick={() => setActiveLink('k')}
+              className={styles['header__nav-link']}
+              href='#'>
+              <span className={`
+              ${styles['header__link-content']}
+              ${activeLink === 'k' ? styles['header__link-content--active'] : ''}
+              `}>
+                Kids
+              </span>
+            </a>
           </li>
         </ul>
         <div>
-        <div className={styles['header__logo-wrapper']}>
-          <Logo className={styles['header__logo']} />
-        </div>
+          <div className={styles['header__logo-wrapper']}>
+            <Logo className={styles['header__logo']} />
+          </div>
         </div>
         <div className={styles['header__icon-group']}>
           <div className={styles['header__icon-wrapper-small']}>
