0933f7fe14195d20a7460dd16804a9f6fe589c2a
ekateriinal <ekateriina.loginova@outlook.com>
2022-04-24 15:00:00 +0300

    ProductItem - added images

--- src/Components/Singleproductitem/Singleproductitem.js
+++ src/Components/Singleproductitem/Singleproductitem.js
@@ -1,33 +1,44 @@
 import styles from "./Singleproductitem.module.scss";
+import product1and5 from '../../images/product1';
+import productTwo from '../../images/product2';
+import product3and6 from '../../images/product6';
+import productFour from '../../images/product4';
+
 const Singleproductitem = () => {
   const products = [
     {
       id: "product1",
+      img: {product1and5},
       title: "Apollo Running Short",
       price: "$50.00",
     },
     {
       id: "product2",
+      img: {productTwo},
       title: "Apollo Dress",
       price: "$100.00",
     },
     {
       id: "product3",
+      img: {product3and6},
       title: "Apollo Trousers",
       price: "$30.00",
     },
     {
       id: "product4",
+      img: {productFour},
       title: "Apollo Man T-shirt",
       price: "$50.00",
     },
     {
       id: "product5",
+      img: {product1and5},
       title: "Apollo Skirt",
       price: "$50.00",
     },
     {
       id: "product6",
+      img: {product3and6},
       title: "Apollo Schildren T-shirt",
       price: "$50.00",
     },
