16841d1d4fe6c53628bd937a2f5a58c03c24f65b
Siiri Kask <kask.siiri@gmail.com>
2022-04-24 15:13:14 +0300

    added singleproductitem info

--- src/Components/Singleproductitem/Singleproductitem.js
+++ src/Components/Singleproductitem/Singleproductitem.js
@@ -1,55 +1,56 @@
 import styles from "./Singleproductitem.module.scss";
-import product1and5 from '../../images/product1';
-import productTwo from '../../images/product2';
-import product3and6 from '../../images/product6';
-import productFour from '../../images/product4';
+import product1and5 from "../../images/product1";
+import productTwo from "../../images/product2";
+import product3and6 from "../../images/product6";
+import productFour from "../../images/product4";
 
 const Singleproductitem = () => {
   const products = [
     {
       id: "product1",
-      img: {product1and5},
+      img: { product1and5 },
       title: "Apollo Running Short",
       price: "$50.00",
     },
     {
       id: "product2",
-      img: {productTwo},
+      img: { productTwo },
       title: "Apollo Dress",
       price: "$100.00",
     },
     {
       id: "product3",
-      img: {product3and6},
+      img: { product3and6 },
       title: "Apollo Trousers",
       price: "$30.00",
     },
     {
       id: "product4",
-      img: {productFour},
+      img: { productFour },
       title: "Apollo Man T-shirt",
       price: "$50.00",
     },
     {
       id: "product5",
-      img: {product1and5},
+      img: { product1and5 },
       title: "Apollo Skirt",
       price: "$50.00",
     },
     {
       id: "product6",
-      img: {product3and6},
-      title: "Apollo Schildren T-shirt",
+      img: { product3and6 },
+      title: "Apollo children T-shirt",
       price: "$50.00",
     },
   ];
 
   return (
-    <ul>
-      <li>
-        <div>
-          <h2>product title</h2>
-          <p>$200</p>
+    <ul className={styles["group"]}>
+      <li className={styles["group__item"]}>
+        <div className={styles["group__product-box"]}>
+          <div className={styles["group__product-img"]}></div>
+          <span className={styles["group__title"]}> title</span>
+          <span className={styles["group__price"]}>$200</span>
         </div>
       </li>
     </ul>
