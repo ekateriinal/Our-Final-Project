ad2b92f8b814dba255159f36e0d9877cc5f9c31b
Siiri Kask <kask.siiri@gmail.com>
2022-04-24 10:28:20 +0300

    App.js - I changed heading

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import './styles/base.scss';
-import './styles/sass-base.scss';
-import styles from './App.module.scss';
-import Heading from './Components/Heading/Heading';
-import Wrapper from './Components/Wrapper/Wrapper';
+import "./styles/base.scss";
+import "./styles/sass-base.scss";
+import styles from "./App.module.scss";
+import Heading from "./Components/Heading/Heading";
+import Wrapper from "./Components/Wrapper/Wrapper";
 
 const App = () => {
   return (
-    <div className={styles['wrapper']}>
-   <Wrapper appearance='light'>
-     <Heading>Hello! Iam on the light wrapper</Heading>
-   </Wrapper>
-   <Wrapper appearance='dark'>
-     <Heading>Its still me on the dark wrapper</Heading>
-   </Wrapper>
-   </div>
-  )
-}
+    <div className={styles["wrapper"]}>
+      <Wrapper appearance="light">
+        <Heading>Hello! Iam on the light wrapper</Heading>
+      </Wrapper>
+      <Wrapper appearance="dark">
+        <Heading>I am the best. Okey it is a joke.</Heading>
+      </Wrapper>
+    </div>
+  );
+};
 
 export default App;
